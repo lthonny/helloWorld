c13d1f3d65262124f302dd853477c6fc075a7e59
Ithonny <thonny02@mail.ru>
2021-05-26 00:20:58 +0300

    ..

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 // 2. You have a string aaabbdcccccf, transform it the following way => a3b2d1c5f1 ie: aabbaa -> a2b2a2 not a4b2
+const arrString = 'aaabbdcccccf';
 
 // 3. В последовательности записаны целые числа от 1 до N в произвольном порядке, но одно из чисел пропущено (остальные встречаются ровно по одному разу). N заранее неизвестно. Определить пропущенное число.
 
 // 4. В последовательности записаны целые числа. Одно из чисел встречается ровно один раз, остальные — по два раза. Найти число, которое встречается один раз.
+const arrNumber = [1, 2, 2, 3, 4, 4, 5, 5, 3];
+
+let newArray = arrNumber.filter((item) => {
+    const counter = 1;
+    return item.length > counter;
+});
+
+console.log(newArray);
+
+// const newArray = arrNumber.reduce((total, current) => {
+//     return current + total;
+// })
+// arrNumber.forEach((value) => {
+//     return arrNumber[value] = (arrNumber[value] || 0) + 1;
+// })
+
+console.log(arrNumber);
+
+
+// arrNumber.filter(item => {
+//     return item.length > item.length;
+// })
+
 
 // 5. isPalindrome(“racecar”); //true
 // isPalindrome(“table”); //false
 // isPalindrome(“А роза упала на лапу Азора”’); //true
+
+// split() разбивает объект String на массив [i n s p i r i t]
+// reverse() переворачивает элементы массива [i n s p i r i t] = [t i r i p s n i]
+// join() объединяет все элементы массива [tiripsni]
+
+//старый формат записи
+function isPalindrom(str) {
+    return str === str.split('').reverse().join('');
+}
+
+console.log(isPalindrom('racecar'));
+
+//новый формат записи
+const isPalindrom = (str) => str === str.split('').reverse().join('');
+console.log(isPalindrom('racecar'));
 
 
 // 6. Написать функцию, которая принимает на вход 2 строки и проверяет являются ли входные строки анаграммами.
@@ -29,7 +68,7 @@
 // [“2 : 2”, “3 : 3”] => 1
 // [“2 : 2”, “2 : 2”] => 2
 // [“3 : 2”, “3 : 2”] => 2
-/
+
 
 
 
