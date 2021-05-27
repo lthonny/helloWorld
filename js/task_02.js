761fbc749c40db5d4f01a954c561168181cf285e
lthonny <msr.kes0@gmail.com>
2021-05-27 12:12:49 +0300

    task4

--- js/task_02.js
+++ js/task_02.js
@@ -1,12 +1,35 @@
 // 2. You have a string aaabbdcccccf, transform it the following way => a3b2d1c5f1 ie: aabbaa -> a2b2a2 not a4b2
-const arrString = 'aabbaa';
-const newArrayString = arrString.split('');  // массив
 
-// const result = newArrayString.reduce((accumlator, current) => {
-//     console.log(current);
-//     return (typeof accumlator[current] !== 'undefined') 
-//       ? { ...accumlator, [current]: accumlator[current] + 1 } 
-//       : { ...accumlator, [current]: 1 }
-// }, {})
 
-console.log(result);
+function transformString(arr) {
+  const arrString = arr.split('');
+  let stringСonversion = ''; 
+  let letter = arrString[0]; // берем элементы по порядку [a, a, a, ]
+  let counter = 1; // счетчик
+
+  for (i = 1; i < arrString.length; i++) {
+    if(arrString[i] === letter) { // если, первый элемент массива [1] ===
+      counter += 1; // 2
+    } else {
+      stringСonversion +=  letter + counter; // строка + буква + количество 'a+a+2' 
+      counter = 1;
+      letter = arrString[i];
+    }
+  }
+
+  stringСonversion += letter + counter;
+  return stringСonversion;
+}
+
+console.log(transformString('aaabbdcccccf'));
+console.log(transformString('aabbaa'));
+console.log(transformString('aasabbdcccchsacf'));
+console.log(transformString('aabsbaa'));
+
+
+
+
+
+
+
+
--- js/task_04.js
+++ js/task_04.js
@@ -1,33 +1,36 @@
 // 4. В последовательности записаны целые числа. Одно из чисел встречается ровно один раз, остальные — по два раза. Найти число, которое встречается один раз.
-const arrNumber = [1, 2, 2, 3, 4, 4, 5, 5, 3, 9, 1];
-const accum = {
-    
-};
+// case tests
+const arrNumber1 = [1, 2, 2, 3, 4, 4, 5, 5, 3, 9, 1];
+const arrNumber2 = [1, 3, 4, 4, 5, 3, 9, 9, 1];
+// const arrNumber3 = [3, 4, 4, 5, 5];
+// const arrNumber4 = [1, 2, 1];
 
-arrNumber.forEach(item => {
-    if (accum[item] === undefined) {
-        accum[item] = 1;
-    } else {
-        accum[item] = accum[item] + 1;
-    }
-});
+const accum = {};
 
-// for (let value of Object.values(accum)) { // узнали значение 
-//     if (value == 1) {   // которое встречается одни раз
-//         console.log(value);
-//         for (let key of Object.keys(accum)) {
-//             if ()
-//         }
-//     }
-// }
+function accumObj(arr) {
+    arr.forEach(item => {
+        if (accum[item] === undefined) {
+            accum[item] = 1;
+        } else {
+            accum[item] = accum[item] + 1;
+        }
+    });
+}
+
+accumObj(arrNumber1);
 
 
-console.log(Object.entries(accum).filter(([key, value]) => {
-    if (value = 1) {
-        console.log(key, value);
-    }
-}));
+(function receivingKeyValue() {
+    Object.entries(accum).filter(([key, value]) => {
+        if (value == 1) {
+            console.log(`'Key': ${key} , 'value': ${value}`);
+        }
+    });
+})();
 
-./
 
-console.log(accum); // [ 1, 2, 3, 4, 5, 9 ]
+
+// РАЗОБРАТЬ TASKS 
+
+
+
