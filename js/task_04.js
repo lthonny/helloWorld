dd8f33b82ee4b1a92009eb29460b78ff6735be28
lthonny <thonny02@mail.ru>
2021-05-31 16:09:03 +0300

    final

--- js/task_04.js
+++ js/task_04.js
@@ -6,92 +6,86 @@ const arrNumber2 = [1, 3, 4, 4, 5, 3, 9, 9, 1];
 const accum = {};
 
 function accumObj(arr) {
-    arr.forEach(item => {
-        if (accum[item] === undefined) {
-            accum[item] = 1;
-        } else {
-            accum[item] = accum[item] + 1;
-        }
-    });
+	arr.forEach(item => {
+		if (accum[item] === undefined) {
+			accum[item] = 1;
+		} else {
+			accum[item] = accum[item] + 1;
+		}
+	});
 }
 
 accumObj(arrNumber1);
 
-
 (function receivingKeyValue() {
-    Object.entries(accum).filter(([key, value]) => {
-        if (value == 1) {
-            console.log(`'Key': ${key} , 'value': ${value}`);
-        }
-    });
+	Object.entries(accum).filter(([key, value]) => {
+		if (value == 1) {
+			console.log(`'Key': ${key} , 'value': ${value}`);
+		}
+	});
 })();
-
-
 
 // optimized 1
 
-const singleNumber = function(nums) {
-    let uniq = new Set(); // save no repeat numbers
-    let uniqSum = 0; // sum no repeat numbers
-    let numSum = 0; // sum all numbers
-    
-    for (let i = 0; i < nums.length; i++) {
-        const current = nums[i];
+const getSingleNumber = function (nums) {
+	let uniq = new Set(); // save no repeat numbers
+	let uniqSum = 0; // sum no repeat numbers
+	let numSum = 0; // sum all numbers
 
-        if (!uniq.has(current)) {
-            uniq.add(current);
-            uniqSum += current;
-        }
-        numSum += current;
-    }
-    return uniqSum * 2 - numSum;
+	for (let i = 0; i < nums.length; i++) {
+		const current = nums[i];
+
+		if (!uniq.has(current)) {
+			uniq.add(current);
+			uniqSum += current;
+		}
+		numSum += current;
+	}
+	return uniqSum * 2 - numSum;
 };
 
 // test cases
 const testCases = [
-    { input: [1, 1, 2, 2, 3]},
-    { input: [4, 3, 2, 2, 3, 5, 5]},
-    { input: [4, 1, 0, 1, 3, 4, 3]},
-    { input: [9, 1, 2, 9, 3, 4, 3, 2, 1, 5, 5]},
-    { input: [5, 1, 1, 2, 2, 4, 8, 4, 3, 3, 5]},
+	{ input: [1, 1, 2, 2, 3] },
+	{ input: [4, 3, 2, 2, 3, 5, 5] },
+	{ input: [4, 1, 0, 1, 3, 4, 3] },
+	{ input: [9, 1, 2, 9, 3, 4, 3, 2, 1, 5, 5] },
+	{ input: [5, 1, 1, 2, 2, 4, 8, 4, 3, 3, 5] },
 ];
 
-testCases.forEach((testCase) => {
-    const {input} = testCase;
-    const noRepeatNumber = singleNumber(input);
-    console.log(`
+testCases.forEach(testCase => {
+	const { input } = testCase;
+	const noRepeatNumber = getSingleNumber(input);
+	console.log(`
         input: ${input} no repeat result: ${noRepeatNumber};
     `);
-})
-
-
+});
 
 // optimized 2
 
-const singleNumber = function(nums) {
-    let uniq = Array.from(new Set(nums)); // no repeat numbers
-    let reduceSum = (s, i) => s + i;
+const singleNumber = function (nums) {
+	let uniq = Array.from(new Set(nums)); // no repeat numbers
+	let reduceSum = (accum, current) => accum + current;
 
-    let uniqSum = uniq.reduce(reduceSum); // sum no repeat numbers
-    let numSum = nums.reduce(reduceSum); // sum all numbers
-    
-    return uniqSum * 2 - numSum;
+	let uniqSum = uniq.reduce(reduceSum); // sum no repeat numbers
+	let numSum = nums.reduce(reduceSum); // sum all numbers
+
+	return uniqSum * 2 - numSum;
 };
-
 
 // test cases
 const testCases = [
-    { input: [1, 1, 2, 2, 3]},
-    { input: [4, 3, 2, 2, 3, 5, 5]},
-    { input: [4, 1, 0, 1, 3, 4, 3]},
-    { input: [9, 1, 2, 9, 3, 4, 3, 2, 1, 5, 5]},
-    { input: [5, 1, 1, 2, 2, 4, 8, 4, 3, 3, 5]},
+	{ input: [1, 1, 2, 2, 3] },
+	{ input: [4, 3, 2, 2, 3, 5, 5] },
+	{ input: [4, 1, 0, 1, 3, 4, 3] },
+	{ input: [9, 1, 2, 9, 3, 4, 3, 2, 1, 5, 5] },
+	{ input: [5, 1, 1, 2, 2, 4, 8, 4, 3, 3, 5] },
 ];
 
-testCases.forEach((testCase) => {
-    const {input} = testCase;
-    const noRepeatNumber = singleNumber(input);
-    console.log(`
+testCases.forEach(testCase => {
+	const { input } = testCase;
+	const noRepeatNumber = singleNumber(input);
+	console.log(`
         input: ${input} no repeat result: ${noRepeatNumber};
     `);
-})
+});
